c1e9b13c1ae1ac4d2003ba44de26df64d63ed339
Nikita <s2015836@x.donstu.ru>
2022-04-14 18:32:57 +0300

    147. add fetching data from Marvel api

---
+++ src/services/MarvelService.js
@@ -0,0 +1,22 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=8c3a4de72b3fc0df9b69371fbb96ea79';
+
+    getResource = async (url) => {
+        const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService;
